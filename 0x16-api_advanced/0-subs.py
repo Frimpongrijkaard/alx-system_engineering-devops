42a135f011cf282504ca45175877cdcfe8af5cf4
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-10-11 20:48:38 +0000

    extract number of sub

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" this script perform extracting of number
+    of subscribers.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """return the number of subscribers """
+
+    headers = {"User-agent" : "myapp_API/0.0.1"}
+    response = requests.get(f"https://oauth.reddit.com/r/{subreddit}/about.json", headers=headers)
+
+    if response.status_code == 200:
+        res = response.json()
+        if 'data' in res['data'] and 'subscribers' in res['subscribers']:
+            return res['data']['subscribers']
+    else:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+solution to advanced api 
